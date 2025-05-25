1524ab9852642fcbf901e5d9ebbb469793c28cd4
Brodzko <martin.brodziansky@gmail.com>
2025-05-26 00:32:37 +0200

    Some level of fetching from gitlab

--- zsh/zsh/gitlab/queries/models.zsh
+++ zsh/zsh/gitlab/queries/models.zsh
@@ -1,22 +1,26 @@
 # Convention: key is how I refer to these values, value is a jq compatible extraction path
 # The input comes from the gql query  (list_mrs.graphql)
-declare -A model_mr=(
-  [iid]=.iid 
-  [draft]=.draft 
-  [title]=.title 
-  [description]=.description
-  [author]=.author.name 
-  [created_at]=.createdAt 
-  [reviewers]='(.reviewers.nodes | map(.name) | join(","))'
-  [approved_by]='(.approvedBy.nodes | map(.name) | join(","))'
-  [source_branch]=.sourceBranch 
-  [target_branch]=.targetBranch 
-  [pipeline_status]=.headPipeline.status 
-  [pipeline_iid]=.headPipeline.iid
-)
+# TODO: Key order guarantee?
+default_to() {
+  echo "($1 // \"$2\" | if . == \"\" then \"$2\" else . end)"
+}
 
+declare -A model_mr=(
+  [iid]=$(default_to '.iid' '-')
+  [draft]=$(default_to '.draft' '-')
+  [title]=$(default_to '.title' '-')
+  [description]=$(default_to '.description' '-')
+  [author]=$(default_to '.author.name' '-')
+  [created_at]=$(default_to '.createdAt' '-')
+  [reviewers]=$(default_to '.reviewers.nodes | map(.name) | join(",")' '-')
+  [approved_by]=$(default_to '.approvedBy.nodes | map(.name) | join(",")' '-')
+  [source_branch]=$(default_to '.sourceBranch' '-')
+  [target_branch]=$(default_to '.targetBranch' '-')
+  [pipeline_status]=$(default_to '.headPipeline.status' '-')
+  [pipeline_iid]=$(default_to '.headPipeline.iid' '-')
+)
 # For extracting using jq, will output fields as tab separated values
 model_mr_paths="${(j:, :)${(@v)model_mr}}"
 
 # For iterating over extracted tab separated values
-model_mr_keys="${(j:\t:)${(@k)model_mr}}"
+model_mr_keys=(${(@k)model_mr})
