d089312baba0227e105c1e4b1965bad1045a40f4
Brodzko <martin.brodziansky@gmail.com>
2025-05-26 00:32:36 +0200

    Render MR detail in fzf preview

--- zsh/fancy_symbols.zsh
+++ zsh/fancy_symbols.zsh
@@ -1,2 +1,12 @@
 export SYM_MR=$'\U00F04C2'
 export SYM_SEPARATOR=$'\u001f'
+export SYM_CI_SUCCESS=$'\uf058'
+export SYM_CI_WARNING=$'\uea6c'
+export SYM_CI_FAILED=$'\uf530'
+export SYM_CI_RUNNING=$'\U00F0996'
+export SYM_CI_CANCELLED=$'\U00F073A'
+export SYM_EYE=$'\uf06e'
+
+export SYM_LINE_HORIZONTAL=$'\uf45b'
+
+export SYM_GIT_COMPARE=$'\uf47f'
--- zsh/gitlab.zsh
+++ zsh/gitlab.zsh
@@ -1,8 +1,9 @@
-ZSHRC_DIR="${0:A:h}"
+export ZSHRC_DIR="${0:A:h}"
 
 source "$ZSHRC_DIR/chalk.zsh"
 source "$ZSHRC_DIR/fancy_symbols.zsh"
 source "$ZSHRC_DIR/utils.zsh"
+source "$ZSHRC_DIR/preview_utils.zsh"
 
 # Gitlab CLI
 alias mr="glab mr view"
@@ -33,6 +34,31 @@ print_mr_listitem() {
   done
 }
 
+fetch_mr_list() {
+  glab mr list --order=updated_at "$@" --output=json | jq -r '.[] | [.iid, .target_branch, .title, .author.name, .state, .draft, .created_at] | @tsv' | print_mr_listitem
+}
+
 mrs() {
-  glab mr list "$@" --output=json | jq -r '.[] | [.iid, .target_branch, .title, .author.name, .state, .draft, .created_at] | @tsv' | print_mr_listitem
+  fetch_mr_list "$@" | fzf --ansi --reverse --info=inline --preview '
+    source "$ZSHRC_DIR/preview_utils.zsh"; \
+    local iid=$(echo "{}" | awk -F"[][]" "{print \$2}"); \
+    glab mr show $iid --output=json | \
+    jq -r ". | [
+                  .iid, 
+                  .source_branch, 
+                  .target_branch, 
+                  .title, 
+                  (.description | if . == \"\" then \"No description\" else . end),
+                  .author.name, 
+                  .state, 
+                  (.draft | tostring // \"false\"), 
+                  .created_at,
+                  (.head_pipeline?.id) // \"-\", 
+                  (.head_pipeline?.status // \"-\"),
+                  (if .reviewers | length > 0 then (.reviewers | map(.name) | join(\",\")) else \"-\" end),
+                  (.has_conflicts | tostring // \"false\"),
+                  (.changes_count // \"-\"),
+                  (if .detailed_merge_status == \"not_approved\" then \"false\" else \"true\" end)
+                ]
+    | @tsv" | print_mr_detail' --preview-window=wrap
 }
