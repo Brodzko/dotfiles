8654ff65b546473266b82e852642e9eecb6adfc5
Brodzko <martin.brodziansky@gmail.com>
2025-05-30 21:56:20 +0200

    Git refactorng

--- zsh/zsh/git/git.zsh
+++ zsh/zsh/git/git.zsh
@@ -1,3 +1,5 @@
+#!/usr/bin/env zsh
+
 source "$ZDOTDIR/chalk.zsh"
 source "$ZDOTDIR/fancy_symbols.zsh"
 source "$ZDOTDIR/utils.zsh"
@@ -83,192 +85,92 @@ get_status_color() {
   fi
 }
 
-_get_status_code() {
-  local input=$1
+# Parse information out of a git status line
+parse_git_status_line() {
+  local line="$1"
+  local full_status staged unstaged rest pathspec oldpath
 
-  print "${input:0:2}"
-}
+  line="${line//$'\n'/}"
 
-_sanitize_path() {
-  local str="$1"
+  [[ -z "$line" ]] && return
 
-  str="${str#[\'\"]}" # Removes a leading ' or "
-  str="${str%[\'\"]}" # Removes a trailing ' or "
-  echo $str
-}
+  full_status="${line:0:2}"
+  staged="${line:0:1}"
+  unstaged="${line:1:1}"
+  rest="${line:3}"
+  rest="${rest#"${rest%%[![:space:]]*}"}" # trim leading whitespace
 
-_get_filepath() {
-  local input=$1
-  # Git code
-  local path_part=${input:3}
+  oldpath=""
+  pathspec="$rest"
 
-  if [[ "$path_part" =~ " -> " ]]; then
-    echo "$(_sanitize_path "${path_part#* -> }")"
+  if [[ "$rest" == \"*\" ]]; then
+    rest="${rest%\"}"
+    rest="${rest#\"}"
+    if [[ "$rest" == *' -> '* ]]; then
+      oldpath="${rest%% -> *}"
+      pathspec="${rest##* -> }"
+    else
+      pathspec="$rest"
+    fi
   else
-    echo "$(_sanitize_path "$path_part")"
+    if [[ "$rest" == *' -> '* ]]; then
+      oldpath="${rest%% -> *}"
+      pathspec="${rest##* -> }"
+    fi
   fi
-}
 
-_get_oldfilepath() {
-  local input=$1
-  local path_part=${input:3}
+  print -r -- "${full_status}"$'\t'"${staged}"$'\t'"${unstaged}"$'\t'"${rest}"$'\t'"${pathspec}"$'\t'"${oldpath}"
 
-  if [[ "$path_part" =~ " -> " ]]; then
-    echo "$(_sanitize_path ${path_part% -> *})"
-  else
-    echo ""
-  fi
 }
 
 enhanced_git_status() {
-  local output=$(git status --porcelain)
+  local status_lines=()
+  local sep=$'\x1f'  # Use a safe delimiter
+  local line output pathspec full_status
 
   while IFS= read -r line; do
-    local xy=$(_get_status_code $line)
-    local filepath=$(_get_filepath $line)
-    local original_filepath=$(_get_oldfilepath $line)
+    output="$(parse_git_status_line "$line")"
+    IFS=$'\t' read -r full_status _ _ _ pathspec _ <<<"$output"
+    status_lines+=("${pathspec}${sep}${line}")
+  done < <(git status --porcelain)
 
-    echo -n "$(chalk $(get_status_color $xy) bold $xy) "
-    if [[ -z "$original_filepath" ]]; then
-      echo -n ""
-    else
-      echo -n "$(chalk magenta $original_filepath) -> "
-    fi
-    echo "$(chalk $(get_status_color $xy) $filepath)"
-
-  done <<<"$output"
+  for entry in "${(on)status_lines[@]}"; do
+    pathspec="${entry%%$sep*}"
+    line="${entry#*$sep}"
+    full_status=$(parse_git_status_line "$line" | awk -F'\t' '{ print $1 }')
+    echo "$(chalk $(get_status_color "$full_status") bold "$line")"
+  done
 }
 
-_generate_diff_command() {
-  local input=$1
+print_preview() {
+  local diff_kind=$1
+  local input_line=$2
 
-  local status_code=$(_get_status_code $input)
-  local filepath=$(_get_filepath $input)
-  local oldfilepath=$(_get_oldfilepath $input)
+  IFS=$'\t' read -r full_status staged unstaged rest pathspec oldpath <<< "$(parse_git_status_line "$input_line")"
 
-  local code_staged="${status_code:0:1}"
-  local code_unstaged="${status_code:1:1}"
+  local diff_status=$(if [[ "$diff_kind" == 'staged' ]]; then echo "$staged"; else echo "$unstaged"; fi)
+  local -a params=()
 
-  local diff_kind=${2:-$(if [[ "$code_unstaged" == " " ]]; then echo "staged"; else echo "unstaged"; fi)}
-  local diff_status=$(if [[ "$code_unstaged" == " " ]]; then echo "$code_staged"; else echo "$code_unstaged"; fi)
-
-  local -a cmd_parts
-
-  if [[ "$diff_status" == "?" ]]; then
-    cmd_parts=(
-      head -n 50
-      "$filepath"
-    )
-
-  elif [[ "$diff_status" == "R" || "$diff_status" == "T" || "$diff_status" == "C" || "$diff_status" == "M" || "$diff_status" == "A" || "$diff_status" == "U" || "$diff_status" == "D" ]]; then
-    cmd_parts=(
-      git diff $(if [[ $diff_kind == "staged" ]]; then echo "--cached"; elif [[ $diff_kind == "both" ]]; then echo "HEAD"; fi)
-      $(if [[ "$diff_status" == "R" || "$diff_status" == "T" || "$diff_status" == "C" ]]; then echo "-M"; fi)
-      " -- "
-      $(if [[ "$diff_status" == "R" || "$diff_status" == "T" || "$diff_status" == "C" ]]; then echo "$oldfilepath"; fi)
-      "$filepath"
-    )
-
-  else
-    cmd_parts=(
-      echo "Unhandled diff status: $diff_status"
-    )
+  if [[ "$diff_kind" == "staged" ]]; then
+    params+=(--cached)
+  elif [[ "$diff_kind" == "both" ]]; then
+    params+=(HEAD)
   fi
 
-  printf "%s\n" "${cmd_parts[@]}"
-}
-
-_generate_diff_format_command() {
-  local input=$1
-  local diff_kind=$2
-
-  local status_code=$(_get_status_code $input)
-  local filepath=$(_get_filepath $input)
-  local oldfilepath=$(_get_oldfilepath $input)
-
-  local code_staged="${status_code:0:1}"
-  local code_unstaged="${status_code:1:1}"
-
-  local diff_status=$(if [[ "$code_unstaged" == " " ]]; then echo "$code_staged"; else echo "$code_unstaged"; fi)
-
-  local -a cmd_parts
-
-  if [[ "$diff_status" == "?" ]]; then
-    cmd_parts=(
-      "bat --color=always"
-    )
-
-  elif [[ "$diff_status" == "R" || "$diff_status" == "T" || "$diff_status" == "C" ]]; then
-    cmd_parts=(
-      "delta --width=$FZF_PREVIEW_COLUMNS"
-    )
-  elif [[ "$diff_status" == "M" || "$diff_status" == "A" || "$diff_status" == "U" || "$diff_status" == "D" ]]; then
-    cmd_parts=(
-      "delta --width=$FZF_PREVIEW_COLUMNS"
-    )
-  else
-    cmd_parts=(
-      echo ""
-    )
+  if [[ "$diff_status" == "R" || "$diff_status" == "T" || "$diff_status" == "C" ]]; then
+    params+=(-M)
   fi
 
-  printf "%s\n" "${cmd_parts[@]}"
-}
-
-_print_preview() {
-  local input=$(cat)
-
-  local -a command=($(_generate_diff_command $input $1))
-  local -a format_command=($(_generate_diff_format_command $input $1))
-
-  echo $(chalk cyan bold italic "$command")
-  echo ""
-  "${command[@]}" | "${format_command[@]}"
-}
-
-show_diff() {
-  local input=$1
-  local diff_kind=$2
-
-  local -a command=($(_generate_diff_command $input $diff_kind))
-
-  "${command[@]}"
-}
-
-stage_file() {
-  local input=$1
-
-  local status_code=$(_get_status_code $input)
-  local filepath=$(_get_filepath $input)
-  local oldfilepath=$(_get_oldfilepath $input)
-
-  local path_to_stage=$(if [[ -z "$oldfilepath" ]]; then echo "$filepath"; else echo "$oldfilepath"; fi)
-
-  local code_unstaged="${status_code:1:1}"
-
-  echo "staging $path_to_stage"
-  if [[ "$code_unstaged" != " " ]]; then
-    git add "$path_to_stage"
+  if [[ $diff_status == "?" ]]; then
+    echo $(chalk cyan bold "head -n 50 \"$pathspec\"")
+    echo ""
+    head -n 50 "$pathspec" | bat --color=always 
+  elif [[ "$diff_status" =~ ^(R|T|C|M|A|U|D| ) ]]; then
+    echo $(chalk cyan bold "git diff ${params[@]} -- $pathspec")
+    echo ""
+    git diff ${params[@]} -- $pathspec | delta --width=$FZF_PREVIEW_COLUMNS
   else
-    echo "Nothing to stage"
-  fi
-}
-
-unstage_file() {
-  local input=$1
-
-  local status_code=$(_get_status_code $input)
-  local filepath=$(_get_filepath $input)
-  local oldfilepath=$(_get_oldfilepath $input)
-
-  local path_to_stage=$(if [[ -z "$oldfilepath" ]]; then echo "$filepath"; else echo "$oldfilepath"; fi)
-
-  local code_staged="${status_code:0:1}"
-
-  if [[ "$code_staged" != " " ]]; then
-    git restore --staged "$path_to_stage"
-  else
-    echo "Nothing to unstage"
+    echo $(chalk red bold "Unhandled case!")
   fi
 }
 
@@ -282,13 +184,13 @@ gst() {
       --multi \
       --preview-window=right,70%,wrap \
       --preview \
-      'source $ZDOTDIR/git/git.zsh; echo {} | _print_preview' \
-      --bind "1:change-preview(source $ZDOTDIR/git/git.zsh; echo {} | _print_preview staged)" \
-      --bind "2:change-preview(source $ZDOTDIR/git/git.zsh; echo {} | _print_preview unstaged)" \
-      --bind "3:change-preview(source $ZDOTDIR/git/git.zsh; echo {} | _print_preview both)" \
-      --bind "0:execute(source $ZDOTDIR/git/git.zsh; show_diff {} staged)" \
-      --bind "9:execute(source $ZDOTDIR/git/git.zsh; show_diff {} unstaged)" \
-      --bind "8:execute(source $ZDOTDIR/git/git.zsh; show_diff {} both)" \
+      'source $ZDOTDIR/git/git.zsh; print_preview unstaged {}' \
+      --bind "1:change-preview(source $ZDOTDIR/git/git.zsh; print_preview staged {})" \
+      --bind "2:change-preview(source $ZDOTDIR/git/git.zsh; print_preview unstaged {})" \
+      --bind "3:change-preview(source $ZDOTDIR/git/git.zsh; print_preview both {})" \
+      --bind "0:execute(source $ZDOTDIR/git/git.zsh; print_preview staged {})" \
+      --bind "9:execute(source $ZDOTDIR/git/git.zsh; print_preview unstaged {})" \
+      --bind "8:execute(source $ZDOTDIR/git/git.zsh; print_preview both {})" \
       --bind "s:execute-silent(source $ZDOTDIR/git/git.zsh; stage_file {})+reload-sync(source $ZDOTDIR/git/git.zsh; enhanced_git_status)" \
       --bind "u:execute-silent(source $ZDOTDIR/git/git.zsh; unstage_file {})+reload-sync(source $ZDOTDIR/git/git.zsh; enhanced_git_status)" \
       --bind "S:execute-silent()+reload(enhanced_git_status)" \
